ed53e02c583f459e3e8c8a03a2e7d4102551b511
tavinzn <otaviosantosandrigo08@gmail.com>
2025-06-06 15:20:48 -0300

    Site finalizado

--- dist/js/script.js
+++
@@ -1,6 +0,0 @@
-const btn = document.querySelector('.themeToggle');
-
-btn.addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme');
-    btn.classList.toggle('active');
-});
